25f1cc4c961604eec872a59d4339fb601e708ee1
ville <>
2009-02-12 19:00:01 +0000

    Lisätty GobDeletionMessage.

---
+++ AssaultWing/Net/Messages/GobDeletionMessage.cs
@@ -0,0 +1,41 @@
+﻿namespace AW2.Net.Messages
+{
+    /// <summary>
+    /// A message from a game server to a game client notifying
+    /// of the deletion of a gob.
+    /// </summary>
+    public class GobDeletionMessage : GameplayMessage
+    {
+        /// <summary>
+        /// Identifier of the message type.
+        /// </summary>
+        protected static MessageType messageType = new MessageType(0x25, false);
+
+        /// <summary>
+        /// Identifier of the gob to delete.
+        /// </summary>
+        public int GobId { get; set; }
+
+        /// <summary>
+        /// Writes the body of the message in serialised form.
+        /// </summary>
+        /// <param name="writer">Writer of serialised data.</param>
+        protected override void Serialize(NetworkBinaryWriter writer)
+        {
+            base.Serialize(writer);
+            // Player controls (request) message structure:
+            // int: gob identifier
+            writer.Write((int)GobId);
+        }
+
+        /// <summary>
+        /// Reads the body of the message from serialised form.
+        /// </summary>
+        /// <param name="reader">Reader of serialised data.</param>
+        protected override void Deserialize(NetworkBinaryReader reader)
+        {
+            base.Deserialize(reader);
+            GobId = reader.ReadInt32();
+        }
+    }
+}

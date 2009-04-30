0856e409b7742a0c06ba5e4eb717aea525c31f8c
ville <>
2009-04-30 17:56:36 +0000

    Lisätty ping- ja pong-viestit

---
+++ AssaultWing/Net/Messages/PingReplyMessage.cs
@@ -0,0 +1,93 @@
+﻿#if DEBUG
+using NUnit.Framework;
+#endif
+using System;
+
+namespace AW2.Net.Messages
+{
+    /// <summary>
+    /// A reply to a message for checking connection quality and response time.
+    /// </summary>
+    public class PingReplyMessage : Message
+    {
+        /// <summary>
+        /// Contents of the message. Valid only for messages created for sending out.
+        /// The contents should be half the contents of the corresponding
+        /// ping request message.
+        /// </summary>
+        public ArraySegment<byte> Contents { private get; set; }
+
+        /// <summary>
+        /// Timestamp received in the message. Valid only for messages received 
+        /// from the network. This is the time of sending the original ping request,
+        /// in real time from start of game instance.
+        /// </summary>
+        public TimeSpan Timestamp { get; private set; }
+
+        /// <summary>
+        /// Identifier of the message type.
+        /// </summary>
+        protected static MessageType messageType = new MessageType(0x00, true);
+
+        /// <summary>
+        /// Writes the body of the message in serialised form.
+        /// </summary>
+        /// <param name="writer">Writer of serialised data.</param>
+        protected override void Serialize(NetworkBinaryWriter writer)
+        {
+            // Ping request message structure (during game):
+            // long: timestamp originally sent in a ping request message
+            // some bytes: content given by an external entity
+            writer.Write(Contents.Array, Contents.Offset, Contents.Count);
+        }
+
+        /// <summary>
+        /// Reads the body of the message from serialised form.
+        /// </summary>
+        /// <param name="reader">Reader of serialised data.</param>
+        protected override void Deserialize(NetworkBinaryReader reader)
+        {
+            Timestamp = TimeSpan.FromTicks(reader.ReadInt64());
+        }
+    }
+
+    #region Unit tests
+#if DEBUG
+    /// <summary>
+    /// Ping message test class.
+    /// </summary>
+    [TestFixture]
+    public class PingTest
+    {
+        /// <summary>
+        /// Tests passing of timestamp through ping messages.
+        /// </summary>
+        [Test]
+        public void TestPingTimestamp()
+        {
+            TimeSpan timestamp = new TimeSpan(12, 34, 56);
+            var ping = new PingRequestMessage();
+            ping.Timestamp = timestamp;
+
+            byte[] pingData = ping.Serialize();
+            byte[] pingHeader = new byte[8];
+            byte[] pingBody = new byte[pingData.Length - 8];
+            Array.Copy(pingData, pingHeader, 8);
+            Array.Copy(pingData, 8, pingBody, 0, pingBody.Length);
+
+            var ping2 = (PingRequestMessage)Message.Deserialize(pingHeader, pingBody, 0);
+            var pong = ping2.GetPingReplyMessage();
+
+            byte[] pongData = pong.Serialize();
+            byte[] pongHeader = new byte[8];
+            byte[] pongBody = new byte[pongData.Length - 8];
+            Array.Copy(pongData, pongHeader, 8);
+            Array.Copy(pongData, 8, pongBody, 0, pongBody.Length);
+
+            var pong2 = (PingReplyMessage)Message.Deserialize(pongHeader, pongBody, 0);
+            Assert.AreEqual(timestamp, pong2.Timestamp);
+        }
+    }
+#endif
+    #endregion Unit tests
+}
---
+++ AssaultWing/Net/Messages/PingRequestMessage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AW2.Net.Messages
+{
+    /// <summary>
+    /// A message for checking connection quality and response time.
+    /// </summary>
+    public class PingRequestMessage : Message
+    {
+        /// <summary>
+        /// Received payload. Used only for deserialised messages.
+        /// </summary>
+        byte[] receivedBytes;
+
+        /// <summary>
+        /// Timestamp to send in the message. This is the time of sending
+        /// the original ping request, in real time from start of game instance.
+        /// </summary>
+        public TimeSpan Timestamp { private get; set; }
+
+        /// <summary>
+        /// Returns a ping reply message corresponding to this received message.
+        /// </summary>
+        public PingReplyMessage GetPingReplyMessage()
+        {
+            if (receivedBytes == null)
+                throw new InvalidOperationException("Cannot create ping reply message for ping request message that has not been received");
+            var reply = new PingReplyMessage();
+            reply.Contents = new ArraySegment<byte>(receivedBytes, 0, receivedBytes.Length / 2);
+            return reply;
+        }
+
+        /// <summary>
+        /// Identifier of the message type.
+        /// </summary>
+        protected static MessageType messageType = new MessageType(0x00, false);
+
+        /// <summary>
+        /// Writes the body of the message in serialised form.
+        /// </summary>
+        /// <param name="writer">Writer of serialised data.</param>
+        protected override void Serialize(NetworkBinaryWriter writer)
+        {
+            // Ping request message structure (during game):
+            // long: ticks of the timestamp
+            // 100 bytes: unspecified data
+            writer.Write((long)Timestamp.Ticks);
+            writer.Write("PINGpingpingping -- pingpingpingping -- pingpingpingping -- pingpingpingping -- pingpingpingping!!!!", 100, false);
+        }
+
+        /// <summary>
+        /// Reads the body of the message from serialised form.
+        /// </summary>
+        /// <param name="reader">Reader of serialised data.</param>
+        protected override void Deserialize(NetworkBinaryReader reader)
+        {
+            receivedBytes = reader.ReadBytes(8 + 100);
+        }
+    }
+}

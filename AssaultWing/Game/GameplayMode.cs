2a4a29167a2b907ae5b0985dd893115b48136c2a
ville <>
2009-05-09 17:27:32 +0000

    Lisätty luokka GameplayMode

---
+++ AssaultWing/Game/GameplayMode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AW2.Game
+{
+    /// <summary>
+    /// A bunch of parameters of the gameplay.
+    /// </summary>
+    public class GameplayMode
+    {
+        /// <summary>
+        /// The types of ship available for selection in the gameplay mode.
+        /// </summary>
+        public string[] ShipTypes { get; set; }
+
+        /// <summary>
+        /// The types of primary weapon available for selection in the gameplay mode.
+        /// </summary>
+        public string[] Weapon1Types { get; set; }
+
+        /// <summary>
+        /// The types of secondary weapon available for selection in the gameplay mode.
+        /// </summary>
+        public string[] Weapon2Types { get; set; }
+    }
+}

62725f8ee21189685610ec33272ccc521b59c542
Ville <Ville@heux>
2009-08-30 21:21:01 +0300

    Puuttuvat fileet lisätty

---
+++ AssaultWing/Game/Gobs/BonusMessage.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using AW2.Helpers;
+
+namespace AW2.Game.Gobs
+{
+    /// <summary>
+    /// Message after bonus is consumed.
+    /// </summary>
+    public class BonusMessage : Gob
+    {
+        private readonly CanonicalString iconBackgroundName = (CanonicalString)"gui_bonuscollect_bg";
+        private Texture2D icon;
+        private Texture2D iconBackground;
+        private SpriteFont messageFont;
+        private static Curve scaleCurve;
+        private static Curve alphaCurve;
+
+        /// <summary>
+        /// Message to display
+        /// </summary>
+        public string Message { set; get; }
+
+        /// <summary>
+        /// Icon to display
+        /// </summary>
+        public string IconName { set; get; }
+
+        /// <summary>
+        /// Names of all textures that this gob type will ever use.
+        /// </summary>
+        public override IEnumerable<CanonicalString> TextureNames
+        {
+            get { return base.TextureNames.Concat(new CanonicalString[] { iconBackgroundName }); }
+        }
+
+        /// <summary>
+        /// Bounding volume of the 3D visuals of the gob, in world coordinates.
+        /// </summary>
+        public override BoundingSphere DrawBounds { get { return new BoundingSphere(); } }
+
+        static BonusMessage()
+        {
+            scaleCurve = new Curve();
+            scaleCurve.Keys.Add(new CurveKey(0, 0.15f));
+            scaleCurve.Keys.Add(new CurveKey(0.3f, 1));
+            scaleCurve.Keys.Add(new CurveKey(0.5f, 1));
+            scaleCurve.ComputeTangents(CurveTangent.Smooth);
+            scaleCurve.PostLoop = CurveLoopType.Constant;
+
+            alphaCurve = new Curve();
+            alphaCurve.Keys.Add(new CurveKey(0, 0));
+            alphaCurve.Keys.Add(new CurveKey(0.2f, 1));
+            alphaCurve.Keys.Add(new CurveKey(2.0f, 1));
+            alphaCurve.Keys.Add(new CurveKey(2.5f, 0));
+            alphaCurve.ComputeTangents(CurveTangent.Linear);
+            alphaCurve.PostLoop = CurveLoopType.Constant;
+        }
+
+        /// <summary>
+        /// Creates an uninitialised bonusmessage.
+        /// </summary>
+        /// This constructor is only for serialisation.
+        public BonusMessage()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Creates a bonusmessage.
+        /// </summary>
+        /// <param name="typeName">The type of the bonusmessage.</param>
+        public BonusMessage(CanonicalString typeName)
+            : base(typeName)
+        {
+        }
+
+        /// <summary>
+        /// Draws the gob's 2D graphics.
+        /// </summary>
+        /// Assumes that the sprite batch has been Begun already and will be
+        /// Ended later by someone else.
+        /// <param name="gameToScreen">Transformation from game coordinates 
+        /// to screen coordinates (pixels).</param>
+        /// <param name="spriteBatch">The sprite batch to draw sprites with.</param>
+        /// <param name="scale">Scale of graphics.</param>
+        public override void Draw2D(Matrix gameToScreen, SpriteBatch spriteBatch, float scale)
+        {
+            Vector2 backgroundPos = Vector2.Transform(Pos, gameToScreen);
+
+            float timePassed = birthTime.SecondsAgo();
+            float finalScale = scaleCurve.Evaluate(timePassed) * scale;
+            Vector2 origin = new Vector2(iconBackground.Width, iconBackground.Height) / 2;
+            Vector2 iconPos = backgroundPos + new Vector2(-origin.X + 6, -icon.Height / 2) * finalScale;
+            Vector2 textPos = backgroundPos + new Vector2(icon.Width + 6, 0) / 2 * finalScale;
+            Vector2 textOrigin = messageFont.MeasureString(Message) / 2;
+            Color drawColor = new Color(1, 1, 1, alphaCurve.Evaluate(timePassed));
+            if (timePassed > scaleCurve.Keys.Last().Position)
+            {
+                textPos = textPos.Round();
+                textOrigin = textOrigin.Round();
+            }
+            spriteBatch.Draw(iconBackground, backgroundPos, null, drawColor,
+                0, origin, finalScale, SpriteEffects.None, 0.2f);
+            spriteBatch.Draw(icon, iconPos, null, drawColor,
+                0, Vector2.Zero, finalScale, SpriteEffects.None, 0.1f);
+            spriteBatch.DrawString(messageFont, Message, textPos, drawColor, 0f, textOrigin, finalScale, SpriteEffects.None, 0f);
+        }
+
+        /// <summary>
+        /// Updates the gob according to physical laws.
+        /// </summary>
+        /// Overriden Update methods should explicitly call this method in order to have 
+        /// physical laws apply to the gob and the gob's exhaust engines updated.
+        public override void Update()
+        {
+            base.Update();
+            float timePassed = birthTime.SecondsAgo();
+            if (alphaCurve.Keys.Last().Position < timePassed)
+                Die(new DeathCause());
+        }
+
+        /// <summary>
+        /// Called when graphics resources need to be loaded.
+        /// </summary>
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            icon = AssaultWing.Instance.Content.Load<Texture2D>(IconName);
+            iconBackground = AssaultWing.Instance.Content.Load<Texture2D>(iconBackgroundName);
+            messageFont = AssaultWing.Instance.Content.Load<SpriteFont>("ConsoleFont");
+        }
+
+        /// <summary>
+        /// Called when graphics resources need to be unloaded.
+        /// </summary>
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+    }
+}
---
+++ AssaultWing/Game/PlayerBonus.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using AW2.Helpers;
+
+namespace AW2.Game
+{
+    /// <summary>
+    /// Bonuses that a player can have.
+    /// </summary>
+    /// This enum is closely related to the enum BonusAction which lists
+    /// what can happen when a bonus is activated.
+    /// <seealso cref="AW2.Game.Gobs.BonusAction"/>
+    [Flags]
+    public enum PlayerBonusTypes : ushort
+    {
+        /// <summary>
+        /// No bonuses
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Primary weapon's load time upgrade
+        /// </summary>
+        Weapon1LoadTime = 0x0001,
+
+        /// <summary>
+        /// Secondary weapon's load time upgrade
+        /// </summary>
+        Weapon2LoadTime = 0x0002,
+
+        /// <summary>
+        /// Primary weapon upgrade
+        /// </summary>
+        /// This bonus is cumulative and the number of accumulated
+        /// primary weapon upgrades is not expressed in these flags.
+        Weapon1Upgrade = 0x0004,
+
+        /// <summary>
+        /// Secondary weapon upgrade
+        /// </summary>
+        /// This bonus is cumulative and the number of accumulated
+        /// secondary weapon upgrades is not expressed in these flags.
+        Weapon2Upgrade = 0x0008,
+    }
+
+    /// <summary>
+    /// PlayerBonus
+    /// </summary>
+    public struct PlayerBonus
+    {
+        static Texture2D bonusIconWeapon1LoadTimeTexture;
+        static Texture2D bonusIconWeapon2LoadTimeTexture;
+
+        /// <summary>
+        /// Type of the bonus.
+        /// </summary>
+        public PlayerBonusTypes Types { get; set; }
+
+        /// <summary>
+        /// Explicit conversion from PlayerBonusTypes.
+        /// </summary>
+        public static explicit operator PlayerBonus(PlayerBonusTypes types)
+        {
+            return new PlayerBonus { Types = types };
+        }
+
+        /// <summary>
+        /// Returns data related to PlayerBonus
+        /// </summary>
+        public PlayerBonusData GetData(Player player)
+        {
+            string bonusText;
+            string bonusIconName;
+            Texture2D bonusIcon;
+
+            var data = AssaultWing.Instance.DataEngine;
+
+            switch (Types)
+            {
+                case PlayerBonusTypes.Weapon1LoadTime:
+                    bonusText = player.Weapon1RealName + "\nspeedloader";
+                    bonusIconName = "b_icon_rapid_fire_1";
+                    bonusIcon = bonusIconWeapon1LoadTimeTexture;
+                    break;
+                case PlayerBonusTypes.Weapon2LoadTime:
+                    bonusText = player.Weapon2RealName + "\nspeedloader";
+                    bonusIconName = "b_icon_rapid_fire_1";
+                    bonusIcon = bonusIconWeapon2LoadTimeTexture;
+                    break;
+                case PlayerBonusTypes.Weapon1Upgrade:
+                    {
+                        Weapon weapon1 = player.Ship != null
+                            ? player.Ship.Weapon1
+                            : (Weapon)data.GetTypeTemplate(TypeTemplateType.Weapon, player.Weapon1RealName);
+                        bonusText = player.Weapon1RealName;
+                        bonusIconName = weapon1.IconName;
+                        bonusIcon = AssaultWing.Instance.Content.Load<Texture2D>(bonusIconName);
+                    }
+                    break;
+                case PlayerBonusTypes.Weapon2Upgrade:
+                    {
+                        Weapon weapon2 = player.Ship != null
+                            ? player.Ship.Weapon2
+                            : (Weapon)data.GetTypeTemplate(TypeTemplateType.Weapon, player.Weapon2RealName);
+                        bonusText = player.Weapon2RealName;
+                        bonusIconName = weapon2.IconName;
+                        bonusIcon = AssaultWing.Instance.Content.Load<Texture2D>(bonusIconName);
+                    }
+                    break;
+                default:
+                    bonusText = "<unknown>";
+                    bonusIconName = "dummytexture";
+                    bonusIcon = AssaultWing.Instance.Content.Load<Texture2D>(bonusIconName);
+                    Log.Write("Warning: Don't know how to draw player bonus box " + Types);
+                    throw new ArgumentException("Don't know how to draw player bonus box " + Types);
+            }
+            return new PlayerBonusData { message = bonusText, icon = bonusIcon, iconName = bonusIconName };
+        }
+
+        /// <summary>
+        /// Called when graphics resources need to be loaded.
+        /// </summary>
+        public static void LoadContent()
+        {
+            var content = AssaultWing.Instance.Content;
+            bonusIconWeapon1LoadTimeTexture = content.Load<Texture2D>("b_icon_rapid_fire_1");
+            bonusIconWeapon2LoadTimeTexture = content.Load<Texture2D>("b_icon_rapid_fire_1");
+        }
+
+        /// <summary>
+        /// Called when graphics resources need to be unloaded.
+        /// </summary>
+        public static void UnloadContent()
+        {
+            // Our textures and fonts are disposed by the graphics engine.
+        }
+
+    }
+
+    /// <summary>
+    /// Message and Icon of PlayerBonus
+    /// </summary>
+    public struct PlayerBonusData
+    {
+        /// <summary>
+        /// Description of the bonus
+        /// </summary>
+        public string message;
+
+        /// <summary>
+        /// Visual description of the bonus
+        /// </summary>
+        public Texture2D icon;
+
+        /// <summary>
+        /// Name of the visual description of the bonus
+        /// </summary>
+        public string iconName;
+    }
+
+}

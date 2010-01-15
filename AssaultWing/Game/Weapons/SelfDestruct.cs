7e63d23522f2a8b71c94b0691d044d214deb787e
Marko <Marko@kinginyellow>
2010-01-16 00:02:22 +0200

    SelfDestruct runko

---
+++ AssaultWing/Game/Weapons/SelfDestruct.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using AW2.Game.Gobs;
+using AW2.Helpers;
+using AW2.Sound;
+using AW2.Game.Particles;
+
+namespace AW2.Game.Weapons
+{
+
+    /// <summary>
+    /// A weapon that shoots gobs forward.
+    /// Each firing can consist of a number of shots being fired.
+    /// The shots are shot at even temporal intervals in a random
+    /// angle. The shot angles distribute evenly in an angular fan
+    /// whose center is directed at the direction of the weapon's owner.
+    /// </summary>
+    public class SelfDestruct : Weapon, IConsistencyCheckable
+    {
+        #region ForwardShot fields
+
+        /// <summary>
+        /// The sound to play when firing.
+        /// </summary>
+        [TypeParameter]
+        string fireSound;
+
+        /// <summary>
+        /// How fast the shots leave the weapon barrel,
+        /// in meters per second.
+        /// </summary>
+        [TypeParameter]
+        float shotSpeed;
+
+        /// <summary>
+        /// Shots produced by this weapon that are still alive.
+        /// </summary>
+        [RuntimeState]
+        List<Gob> liveShots;
+
+        #endregion SelfDestruct fields
+
+        /// <summary>
+        /// Creates an uninitialised forward shooting weapon.
+        /// </summary>
+        /// This constructor is only for serialisation.
+        public SelfDestruct()
+            : base()
+        {
+            fireSound = "Pistol";
+            shotSpeed = 300f;
+            liveShots = new List<Gob>();
+        }
+
+        public SelfDestruct(CanonicalString typeName)
+            : base(typeName)
+        {
+            liveShots = new List<Gob>();
+        }
+
+        /// <summary>
+        /// Fires the weapon.
+        /// </summary>
+        public override void Fire(AW2.UI.ControlState triggerState)
+        {
+            
+        }
+
+        public override void Activate()
+        {
+            FireMode = FireModeType.Single;
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Releases all resources allocated by the weapon.
+        /// </summary>
+        public override void Dispose()
+        {
+        }
+
+        #region IConsistencyCheckable Members
+
+        /// <summary>
+        /// Makes the instance consistent in respect of fields marked with a
+        /// limitation attribute.
+        /// </summary>
+        /// <param name="limitationAttribute">Check only fields marked with 
+        /// this limitation attribute.</param>
+        /// <see cref="Serialization"/>
+        public void MakeConsistent(Type limitationAttribute)
+        {
+            if (limitationAttribute == typeof(TypeParameterAttribute))
+            {
+                /*
+                shotCount = Math.Max(1, shotCount);
+                shotSpacing = Math.Max(0, shotSpacing);
+                 * */
+            }
+        }
+
+        #endregion
+    }
+}

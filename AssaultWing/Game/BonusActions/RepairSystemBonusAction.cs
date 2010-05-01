b59caaa1782f7f301c636694c6b3fb7184177cfe
Ville <ville.v.nurmi@gmail.com>
2010-05-01 22:04:16 +0300

    Bugfix: Repair system ei korjannut

--- AssaultWing/Game/BonusActions/RepairSystemBonusAction.cs
+++ AssaultWing/Game/BonusActions/RepairSystemBonusAction.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework.Graphics;
-using AW2.Helpers;
 using AW2.Game.Gobs;
+using AW2.Helpers;
 
 namespace AW2.Game.BonusActions
 {
     class RepairSystemBonusAction : GameAction
     {
-        static readonly TimeSpan updateCycle = TimeSpan.FromSeconds(1);
-        TimeSpan lastActivatedTime;
+        private static readonly TimeSpan UPDATE_CYCLE = TimeSpan.FromSeconds(1);
+        private TimeSpan _lastActivatedTime;
 
-        /// <summary>
-        /// Action method. Contains logic for enabling the action
-        /// </summary>
         public override void DoAction(float duration)
         {
             base.DoAction(duration);
@@ -22,37 +19,22 @@ namespace AW2.Game.BonusActions
             SetActionMessage();
         }
 
-        /// <summary>
-        /// Enables the ActionMessage (used in BonusOverlay)
-        /// </summary>
         private void SetActionMessage()
         {
             bonusIcon = AssaultWing.Instance.Content.Load<Texture2D>(bonusIconName);
         }
 
-        /// <summary>
-        /// Actions that do something when active
-        /// </summary>
         public override void Update()
         {
-            if (AssaultWing.Instance.GameTime.TotalArenaTime >= lastActivatedTime + updateCycle)
-            {
+            if (AssaultWing.Instance.GameTime.TotalArenaTime >= _lastActivatedTime + UPDATE_CYCLE)
                 GiveHealth();
-            }
         }
 
-        /// <summary>
-        /// This method gives the health to the player
-        /// </summary>
         private void GiveHealth()
         {
-            lastActivatedTime = AssaultWing.Instance.GameTime.TotalArenaTime;
+            _lastActivatedTime = AssaultWing.Instance.GameTime.TotalArenaTime;
             float healthBonus = player.Ship.MaxDamageLevel * -0.05f;
             player.Ship.InflictDamage(healthBonus, new DeathCause());
-            if (healthBonus <= player.Ship.DamageLevel)
-                player.Ship.DamageLevel -= healthBonus;
-            else
-                player.Ship.DamageLevel = 0.0f;
         }
     }
 }

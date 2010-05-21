9ce4c1fc4b517c59e1f7fa660f96c7f552e4aa23
Ville <ville.v.nurmi@gmail.com>
2010-05-21 22:09:56 +0300

    Nimetty RepairSystemBonusAction -> DamageBuffBonusAction

---
+++ AssaultWing/Game/BonusActions/DamageBuffBonusAction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AW2.Game.GobUtils;
+using AW2.Helpers;
+
+namespace AW2.Game.BonusActions
+{
+    [GameActionType(2)]
+    [LimitedSerialization]
+    public class DamageBuffBonusAction : GameAction
+    {
+        [TypeParameter]
+        private string _buffName;
+
+        [TypeParameter]
+        private float _damagePerSecond;
+
+        /// <summary>
+        /// This constructor is only for serialization.
+        /// </summary>
+        public DamageBuffBonusAction()
+        {
+            _buffName = "dummy damage buff";
+            _damagePerSecond = 500;
+        }
+
+        public override void DoAction()
+        {
+            SetActionMessage();
+            base.DoAction();
+        }
+
+        public override void Update()
+        {
+            float damage = AssaultWing.Instance.PhysicsEngine.ApplyChange(_damagePerSecond, AssaultWing.Instance.GameTime.ElapsedGameTime);
+            Player.Ship.InflictDamage(damage, new DeathCause(Player.Ship, DeathCauseType.Damage));
+        }
+
+        private void SetActionMessage()
+        {
+            BonusText = _buffName;
+            BonusIconName = "b_icon_general";
+        }
+    }
+}

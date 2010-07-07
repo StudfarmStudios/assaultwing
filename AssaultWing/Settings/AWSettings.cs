8769ca1df1d47ab93b5f48035462f5227ae248ce
Ville <ville.v.nurmi@gmail.com>
2010-07-07 15:02:40 +0300

    Pelin asetustiedosto siirretty ClickOncen data-hakemistoon levitysversioissa

--- AssaultWing/Settings/AWSettings.cs
+++ AssaultWing/Settings/AWSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Deployment.Application;
 using System.IO;
 using AW2.Helpers;
 
@@ -6,17 +7,38 @@ namespace AW2.Settings
 {
     public class AWSettings
     {
-        private static readonly string SETTINGS_FILENAME = "AssaultWing.config";
         private SoundSettings _sound;
         private NetSettings _net;
 
         public SoundSettings Sound { get { return _sound; } private set { _sound = value; } }
         public NetSettings Net { get { return _net; } private set { _net = value; } }
 
+        private static string SettingsDirectory
+        {
+            get
+            {
+                if (ApplicationDeployment.IsNetworkDeployed)
+                    return ApplicationDeployment.CurrentDeployment.DataDirectory;
+                else
+                {
+                    var assemblyFilename = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    return Path.GetDirectoryName(assemblyFilename);
+                }
+            }
+        }
+
+        private static string SettingsFilename
+        {
+            get
+            {
+                return Path.Combine(SettingsDirectory, "AssaultWing.config");
+            }
+        }
+
         public static AWSettings FromFile()
         {
-            if (File.Exists(SETTINGS_FILENAME))
-                return (AWSettings)TypeLoader.LoadTemplate(SETTINGS_FILENAME, typeof(AWSettings), null);
+            if (File.Exists(SettingsFilename))
+                return (AWSettings)TypeLoader.LoadTemplate(SettingsFilename, typeof(AWSettings), null);
 
             // Create a new settings file
             var settings = new AWSettings();
@@ -32,7 +54,7 @@ namespace AW2.Settings
 
         public void ToFile()
         {
-            TypeLoader.SaveTemplate(this, SETTINGS_FILENAME, typeof(AWSettings), null);
+            TypeLoader.SaveTemplate(this, SettingsFilename, typeof(AWSettings), null);
         }
     }
 }

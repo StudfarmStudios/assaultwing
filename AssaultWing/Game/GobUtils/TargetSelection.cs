e81b270b1df902491026f687425aebe7d0ebbe73
Ville <ville.v.nurmi@gmail.com>
2010-12-25 18:13:07 +0200

    Added missing file

---
+++ AssaultWing/Game/GobUtils/TargetSelection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AW2.Helpers;
+
+namespace AW2.Game.GobUtils
+{
+    /// <summary>
+    /// Selects targets for weapons and semi-intelligent bullets of weapons.
+    /// </summary>
+    public class TargetSelection
+    {
+        /// <summary>
+        /// Chooses a target, preferring those that are
+        /// 1. enemies or at least not friends, and
+        /// 2. straight ahead, and
+        /// 3. close
+        /// </summary>
+        public static Gob ChooseTarget(IEnumerable<Gob> candidates, Gob source, float maxRange)
+        {
+            var targets =
+                from gob in candidates
+                where gob.IsDamageable && !gob.Disabled && gob != source
+                let ownerWeight = gob.Owner == source.Owner ? 5f : gob.Owner == null ? 1f : 0.5f
+                let relativePos = (gob.Pos - source.Pos).Rotate(-source.Rotation)
+                let distanceSquared = relativePos.LengthSquared()
+                where distanceSquared <= maxRange * maxRange && relativePos.X >= 0
+                orderby ownerWeight * (relativePos.X + 5 * Math.Abs(relativePos.Y)) ascending
+                select gob;
+            return targets.FirstOrDefault();
+        }
+    }
+}

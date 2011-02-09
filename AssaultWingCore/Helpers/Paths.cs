6696fc51e6338c185f817158e44adc15cebf4de4
Ville <ville.v.nurmi@gmail.com>
2011-02-09 21:32:33 +0200

    Renamed projects AssaultWing -> AssaultWingCore, and AssaultWingGame -> AssaultWing, and content projects similarly

---
+++ AssaultWingCore/Helpers/Paths.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AW2.Helpers
+{
+    /// <summary>
+    /// Contains relevant directory paths.
+    /// </summary>
+    public static class Paths
+    {
+        /// <summary>
+        /// Root path for all content.
+        /// </summary>
+        public static readonly string CONTENT = @"CoreContent";
+
+        /// <summary>
+        /// Path to arenas.
+        /// </summary>
+        public static readonly string ARENAS = @"CoreContent\arenas";
+
+        /// <summary>
+        /// Path to 3D shaders.
+        /// </summary>
+        public static readonly string SHADERS = @"CoreContent\effects";
+
+        /// <summary>
+        /// Path to fonts.
+        /// </summary>
+        public static readonly string FONTS = @"CoreContent\fonts";
+
+        /// <summary>
+        /// Path to gobs.
+        /// </summary>
+        public static readonly string GOBS = @"CoreContent\gobdefs";
+
+        /// <summary>
+        /// Path to 3D models.
+        /// </summary>
+        public static readonly string MODELS = @"CoreContent\models";
+
+        /// <summary>
+        /// Path to music.
+        /// </summary>
+        public static readonly string MUSIC = @"CoreContent\music";
+
+        /// <summary>
+        /// Path to particle effects.
+        /// </summary>
+        public static readonly string PARTICLES = @"CoreContent\particledefs";
+
+        /// <summary>
+        /// Path to textures.
+        /// </summary>
+        public static readonly string TEXTURES = @"CoreContent\textures";
+
+        /// <summary>
+        /// Path to ship devices.
+        /// </summary>
+        public static readonly string DEVICES = @"CoreContent\weapondefs";
+
+        /// <summary>
+        /// Path to sound effects and music.
+        /// </summary>
+        public static readonly string SOUNDS = @"CoreContent\sounds";
+
+        /// <summary>
+        /// Path to videos.
+        /// </summary>
+        public static readonly string VIDEO = @"CoreContent\video";
+    }
+}
--- AssaultWingCore/Properties/AssemblyInfo.cs
+++ AssaultWingCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assault Wing")]
-[assembly: AssemblyProduct("Assault Wing")]
-[assembly: AssemblyDescription("Fast-paced multiplayer online shoot'em up")]
+[assembly: AssemblyTitle("Assault Wing core")]
+[assembly: AssemblyProduct("Assault Wing core")]
+[assembly: AssemblyDescription("Core functionality of Assault Wing")]
 [assembly: AssemblyCompany("Studfarm Studios")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // On Windows, the following GUID is for the ID of the typelib if this
 // project is exposed to COM. On other platforms, it unique identifies the
 // title storage container when deploying this assembly to the device.
-[assembly: Guid("3496aa49-1ea2-40c8-9afb-d63947c68056")]
+[assembly: Guid("fec3af93-5d22-42fa-9293-c5b0bc3b5b95")]
 
 // Version information for an assembly consists of the following four values:
 //

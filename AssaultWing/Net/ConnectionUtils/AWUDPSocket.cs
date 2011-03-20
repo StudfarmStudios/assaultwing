7f3fe9138c2a5311bbec3171625aebfc449c7772
Ville <ville.v.nurmi@gmail.com>
2011-03-20 19:06:46 +0200

    Fixed UDP reception halt when received message header was invalid

--- AssaultWing/Net/ConnectionUtils/AWUDPSocket.cs
+++ AssaultWing/Net/ConnectionUtils/AWUDPSocket.cs
@@ -42,26 +42,40 @@ namespace AW2.Net.ConnectionUtils
         private void ReceiveFromCompleted(object sender, SocketAsyncEventArgs args)
         {
             // This method is called in a background thread. Catch and custom report all exceptions.
-            try
+            bool isPending = false;
+            do
             {
-                bool isPending = false;
-                do
+                try
                 {
                     CheckSocketError(args);
+                    // Note: UDP reception will stop indefinitely if there is a SocketError.
+                    // This should be okay, assuming SocketError signals that the UDP socket is unusable.
                     if (args.SocketError != SocketError.Success) break;
                     var bufferSegment = new ArraySegment<byte>(args.Buffer, 0, args.BytesTransferred);
                     var endPoint = (IPEndPoint)args.RemoteEndPoint;
                     _messageHandler(bufferSegment, endPoint);
+                }
+                catch (Exception e)
+                {
+                    var message = string.Format("{0} in ReceiveFromCompleted: {1}", e.GetType().ToString(),
+                        e is SocketException ? ((SocketException)e).SocketErrorCode.ToString() : e.ToString());
+                    Errors.Do(queue => queue.Enqueue(message));
+                }
+                try
+                {
                     UseSocket(socket => isPending = socket.ReceiveFromAsync(args));
                 }
-                while (!isPending && !IsDisposed);
+                catch (Exception e)
+                {
+                    var message = string.Format("{0} in ReceiveFromCompleted: {1}", e.GetType().ToString(),
+                        e is SocketException ? ((SocketException)e).SocketErrorCode.ToString() : e.ToString());
+                    Errors.Do(queue => queue.Enqueue(message));
+                    // Note: UDP reception will stop indefinitely because we don't call ReceiveFromAsync any more.
+                    // This hasn't happened ever(?) but if it did, it would probably be due to a SocketException
+                    // which would mean that something is dramatically wrong. So maybe it's okay this way.
+                }
             }
-            catch (Exception e)
-            {
-                var message = string.Format("{0} in ReceiveFromCompleted: {1}", e.GetType().ToString(),
-                    e is SocketException ? ((SocketException)e).SocketErrorCode.ToString() : e.ToString());
-                Errors.Do(queue => queue.Enqueue(message));
-            }
+            while (!isPending && !IsDisposed);
         }
     }
 }

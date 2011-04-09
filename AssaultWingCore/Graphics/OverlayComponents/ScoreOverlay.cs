51d2f3078499e6e02e699ff764998adfdfb4371b
Pekka <pekka.palmu@gmail.com>
2011-04-09 15:43:08 +0300

    Player list fixes

--- AssaultWingCore/Graphics/OverlayComponents/ScoreOverlay.cs
+++ AssaultWingCore/Graphics/OverlayComponents/ScoreOverlay.cs
@@ -12,11 +12,12 @@ namespace AW2.Graphics.OverlayComponents
         private Player _player;
         private Texture2D _scoreBackgroundTexture;
         private SpriteFont _scoreFont;
-        private int scoreLineSpacing = 12;
+        private SpriteFont _textFont;
+        private int scoreLineSpacing = 15;
 
         public override Point Dimensions
         {
-            get { return new Point(_scoreBackgroundTexture.Width, 10 + (AssaultWingCore.Instance.DataEngine.Players.Count() * scoreLineSpacing)); }
+            get { return new Point(_scoreBackgroundTexture.Width, 30 + (AssaultWingCore.Instance.DataEngine.Players.Count() * scoreLineSpacing)); }
         }
 
         public ScoreOverlay(PlayerViewport viewport)
@@ -30,10 +31,10 @@ namespace AW2.Graphics.OverlayComponents
             // Draw scoredisplay background
             spriteBatch.Draw(_scoreBackgroundTexture, Vector2.Zero, Color.White);
 
-            var textTopLeft = new Vector2(5, 10);
-            int numberColumnWidth = 16;
-            int playerNameWidth = 115;
-            int textMargin = 10;
+            var textTopLeft = new Vector2(15, 29);
+            int playerNameWidth = 134;
+            int scoreWidth = 51;
+            int scoreEntryWidth = 35;
 
             var standings = AssaultWingCore.Instance.DataEngine.GameplayMode.GetStandings(AssaultWingCore.Instance.DataEngine.Players);
             int line = 0;
@@ -41,24 +42,39 @@ namespace AW2.Graphics.OverlayComponents
             foreach (var entry in standings)
             {
                 var currentStanding = line + 1;
-                var standingSize = _scoreFont.MeasureString(currentStanding.ToString());
-                var standingPos = textTopLeft + new Vector2(numberColumnWidth - standingSize.X, line * scoreLineSpacing);
-                var playerNamePos = textTopLeft + new Vector2(numberColumnWidth + textMargin, line * scoreLineSpacing);
-                var scorePos = textTopLeft + new Vector2(numberColumnWidth + (textMargin * 2) + playerNameWidth, line * scoreLineSpacing);
-                var scoreText = string.Format("{0} = {1}-{2}", entry.Score, entry.Kills, entry.Suicides);
+                var playerNamePos = textTopLeft + new Vector2(0, line * scoreLineSpacing);
+                var scorePos = textTopLeft + new Vector2(playerNameWidth, line * scoreLineSpacing - 4);
+                var killsPos = textTopLeft + new Vector2(playerNameWidth + scoreWidth, line * scoreLineSpacing);
+                var deathsPos = textTopLeft + new Vector2(playerNameWidth + scoreWidth + scoreEntryWidth, line * scoreLineSpacing);
+                var scoreText = entry.Score.ToString();
+                var killsText = entry.Kills.ToString();
+                var deathsText = entry.Suicides.ToString();
                 var rowColor = _player.ID == entry.SpectatorId ? Color.White : entry.PlayerColor;
-                spriteBatch.DrawString(_scoreFont, currentStanding.ToString(), standingPos.Round(), rowColor);
-                spriteBatch.DrawString(_scoreFont, entry.Name, playerNamePos.Round(), rowColor);
-                spriteBatch.DrawString(_scoreFont, scoreText, scorePos.Round(), rowColor);
+
+                DrawBorderedText(entry.Name, playerNamePos.Round(), _textFont, spriteBatch, rowColor);
+                DrawBorderedText(scoreText, scorePos.Round(), _scoreFont, spriteBatch, rowColor);
+                DrawBorderedText(killsText, killsPos.Round(), _textFont, spriteBatch, rowColor);
+                DrawBorderedText(deathsText, deathsPos.Round(), _textFont, spriteBatch, rowColor);
+
                 ++line;
             }
+        }
+
+        private void DrawBorderedText(string text, Vector2 position, SpriteFont font, SpriteBatch batch, Color color)
+        {
+            batch.DrawString(font, text, position - Vector2.One, Color.Black);
+            batch.DrawString(font, text, position + new Vector2(1, -1), Color.Black);
+            batch.DrawString(font, text, position + new Vector2(-1, +1), Color.Black);
+            batch.DrawString(font, text, position + Vector2.One, Color.Black);
+            batch.DrawString(font, text, position, color);
         }
 
         public override void LoadContent()
         {
             var content = AssaultWingCore.Instance.Content;
-            _scoreBackgroundTexture = content.Load<Texture2D>("gui_radar_bg");
-            _scoreFont = content.Load<SpriteFont>("ConsoleFont");
+            _scoreBackgroundTexture = content.Load<Texture2D>("gui_playerlist_bg");
+            _textFont = content.Load<SpriteFont>("ConsoleFont");
+            _scoreFont = content.Load<SpriteFont>("ScoreFont");
         }
     }
 }

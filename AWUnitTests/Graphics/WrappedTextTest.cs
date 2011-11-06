805cba5eb380a8bd2a8e596dc7023aa9db225f65
Ville <ville.v.nurmi@gmail.com>
2011-11-06 02:08:40 +0200

    Added WrappedText class with unit tests

---
+++ AWUnitTests/Graphics/WrappedTextTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AW2.Graphics
+{
+    [TestFixture]
+    public class WrappedTextTest
+    {
+        private Func<string, float> _getStringWidth;
+
+        [SetUp]
+        public void Setup()
+        {
+            _getStringWidth = x =>
+            {
+                var narrowChars = x.Count(ch => ch == 'i');
+                var wideChars = x.Count(ch => ch == 'W');
+                var normalChars = x.Length - narrowChars - wideChars;
+                return narrowChars * 2.2f + wideChars * 11 + normalChars * 5.5f;
+            };
+        }
+
+        [Test]
+        public void TestFitsOnOneLine()
+        {
+            var text = new WrappedText("foo", _getStringWidth);
+            var lines = text.WrapToWidth(100).ToArray();
+            Assert.AreEqual(1, lines.Length);
+            Assert.AreEqual("foo", lines[0]);
+        }
+
+        [Test]
+        public void TestForcedWrap()
+        {
+            var text = new WrappedText("foofoo", _getStringWidth);
+            var lines20 = text.WrapToWidth(20).ToArray();
+            Assert.AreEqual(1, lines20.Length);
+            Assert.AreEqual("foofoo", lines20[0]);
+        }
+
+        [Test]
+        public void TestWrapAtSpace()
+        {
+            var niceText = new WrappedText("foo foo foo foo foo", _getStringWidth);
+            var niceLines = niceText.WrapToWidth(40).ToArray();
+            Assert.AreEqual(3, niceLines.Length);
+            Assert.AreEqual("foo foo", niceLines[0]);
+            Assert.AreEqual("foo foo", niceLines[1]);
+            Assert.AreEqual("foo", niceLines[2]);
+            var longSpaceText = new WrappedText("   foo   foo   foo         ", _getStringWidth);
+            var longSpaceLines = longSpaceText.WrapToWidth(49.5f).ToArray();
+            Assert.AreEqual(2, longSpaceLines.Length);
+            Assert.AreEqual("foo   foo", longSpaceLines[0]);
+            Assert.AreEqual("foo", longSpaceLines[1].TrimEnd());
+        }
+
+        [Test]
+        public void TestNarrowChars()
+        {
+            var text = new WrappedText("iii iii xxx xxx", _getStringWidth);
+            var lines = text.WrapToWidth(19).ToArray();
+            Assert.AreEqual(3, lines.Length);
+            Assert.AreEqual("iii iii", lines[0]);
+            Assert.AreEqual("xxx", lines[1]);
+            Assert.AreEqual("xxx", lines[2]);
+            var longText = new WrappedText("iii iii iii iii iii", _getStringWidth);
+            var longLines = longText.WrapToWidth(55).ToArray();
+            Assert.AreEqual(1, longLines.Length);
+            Assert.AreEqual("iii iii iii iii iii", longLines[0]);
+        }
+
+        [Test]
+        public void TestWideChars()
+        {
+            var text = new WrappedText("WWW WWW WWW WWW", _getStringWidth);
+            var lines = text.WrapToWidth(71.5f).ToArray();
+            Assert.AreEqual(2, lines.Length);
+            Assert.AreEqual("WWW WWW", lines[0]);
+            Assert.AreEqual("WWW WWW", lines[1]);
+        }
+    }
+}
---
+++ AssaultWingCore/Graphics/WrappedText.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AW2.Graphics
+{
+    /// <summary>
+    /// A piece of text that wraps nicely into lines of desired width.
+    /// </summary>
+    public class WrappedText
+    {
+        private string _text;
+        private Func<string, float> _getStringWidth;
+        private float _enWidth;
+
+        public WrappedText(string text, Func<string, float> getStringWidth)
+        {
+            _text = text;
+            _getStringWidth = getStringWidth;
+            _enWidth = getStringWidth("N");
+        }
+
+        public IEnumerable<string> WrapToWidth(float width)
+        {
+            for (int startIndex = GetNextWordStart(-1); startIndex < _text.Length; )
+            {
+                var endIndex = GetPreviousWordEnd(startIndex, startIndex + (int)(width / _enWidth)); // exclusive index
+                if (endIndex == startIndex) endIndex = GetNextWordEnd(endIndex);
+                var line = _text.Substring(startIndex, endIndex - startIndex);
+                var guessOverflow = _getStringWidth(line) - width;
+                while (endIndex < _text.Length)
+                {
+                    var newEndIndex = guessOverflow > 0
+                        ? GetPreviousWordEnd(startIndex, endIndex)
+                        : GetNextWordEnd(endIndex);
+                    var newLine = _text.Substring(startIndex, newEndIndex - startIndex);
+                    var newGuessOverflow = _getStringWidth(newLine) - width;
+                    if (guessOverflow <= 0 && newGuessOverflow > 0) break;
+                    line = newLine;
+                    endIndex = newEndIndex;
+                    guessOverflow = newGuessOverflow;
+                    if (guessOverflow > 0 && newGuessOverflow <= 0) break;
+                }
+                yield return line;
+                startIndex = GetNextWordStart(endIndex);
+            }
+        }
+
+        public override string ToString()
+        {
+            return _text;
+        }
+
+        private int GetPreviousWordEnd(int startIndex, int i)
+        {
+            i = Math.Min(i, _text.Length);
+            while (i > startIndex && !char.IsWhiteSpace(_text[i - 1])) i--;
+            while (i > startIndex && char.IsWhiteSpace(_text[i - 1])) i--;
+            return i;
+        }
+
+        private int GetNextWordEnd(int i)
+        {
+            while (i < _text.Length && char.IsWhiteSpace(_text[i])) i++;
+            while (i < _text.Length && !char.IsWhiteSpace(_text[i])) i++;
+            return i;
+        }
+
+        private int GetNextWordStart(int i)
+        {
+            if (i < 0)
+                i = 0;
+            else
+                while (i < _text.Length && !char.IsWhiteSpace(_text[i])) i++;
+            while (i < _text.Length && char.IsWhiteSpace(_text[i])) i++;
+            return i;
+        }
+    }
+}

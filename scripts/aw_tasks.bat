35178035ff507de4764f1c7d7d8963744eb6e893
Ville <ville.v.nurmi@gmail.com>
2011-12-30 14:59:11 +0200

    Fixed or fine-tuned task scripts

--- scripts/aw_tasks.bat
+++ scripts/aw_tasks.bat
@@ -2,14 +2,16 @@
 
 :: Creates Windows tasks for managing an official Assault Wing dedicated server.
 :: Note that Windows tasks persist over reboots. Therefore it suffices to run this
-:: batch file just once for a server. See schtasks /? for more info.
+:: batch file just once for a server. See schtasks /? for more info. To delete a task,
+:: type e.g. schtasks /delete /tn "AW config"
 ::
 :: "AW keepalive" relaunches the dedicated server if AssaultWing.exe crashes.
-:: "AW bots" switches bots off for Wednesdays.
+:: "AW config" switches bots off for Wednesdays.
 
 set AW_LAUNCH_BAT=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Studfarm Studios\Assault Wing Dedicated Server.cmd
 set AW_CRASH_EVENT_XPATH=*[System[Provider[@Name='Application Error'] and (Level=2) and (EventID=1000)] and EventData[Data='AssaultWing.exe']]
+set AW_UPDATE_CONFIG=%~dp0%aw_update_config.bat
 
 schtasks /create /f /tn "AW keepalive" /tr "\"%AW_LAUNCH_BAT%\"" /sc ONEVENT /delay 0000:05 /ec Application /mo "%AW_CRASH_EVENT_XPATH%"
 
-schtasks /create /f /tn "AW bots" /tr "ruby %~dp0%aw_update_config.rb" /sc HOURLY
+schtasks /create /f /tn "AW config" /tr "\"%AW_UPDATE_CONFIG%\"" /sc HOURLY /mo 1
---
+++ scripts/aw_update_config.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running %~dpf0
+ruby %~dp0%aw_update_config.rb
+timeout /t 10

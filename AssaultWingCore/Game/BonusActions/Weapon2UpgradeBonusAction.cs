8f5646fa89becbcbf88a0ae8c4b4df55497606ae
Ville <ville.v.nurmi@gmail.com>
2012-02-06 22:04:45 +0200

    Fixed rare NullReferenceException on client join

--- AssaultWingCore/Game/BonusActions/Weapon2UpgradeBonusAction.cs
+++ AssaultWingCore/Game/BonusActions/Weapon2UpgradeBonusAction.cs
@@ -71,8 +71,10 @@ namespace AW2.Game.BonusActions
         public override void Dispose()
         {
             if (Host != null && Host.Owner != null)
+            {
                 Host.SetDeviceType(Weapon.OwnerHandleType.SecondaryWeapon, Host.Owner.Weapon2Name);
-            if (_effectName != "") Host.Owner.PostprocessEffectNames.Remove(_effectName);
+                if (_effectName != "") Host.Owner.PostprocessEffectNames.Remove(_effectName);
+            }
             base.Dispose();
         }
     }

186c69d71ed7235659aaccf2c25a33d27eefc575
Ville <ville.v.nurmi@gmail.com>
2012-02-18 17:08:59 +0200

    Don't increment ApplicationRevision if publish fails

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -7,6 +7,11 @@ pause
 SET ROOT=%~dp0%..
 set FLAVOUR=%1
 call msbuild "%ROOT%\AssaultWing.sln" /target:Publish /p:Configuration=Release%FLAVOUR% /p:Platform=x86
+if ERRORLEVEL 1 goto :error
 start "" "%ROOT%\AssaultWing\bin\x86\Release%FLAVOUR%\app.publish"
 ruby "%~dp0%aw_project_file.rb" //ApplicationRevision ++
-pause
+goto :eof
+echo Publish succeeded
+
+:error
+echo Publish failed

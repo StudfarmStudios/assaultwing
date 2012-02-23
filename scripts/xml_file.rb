73c994cb97013751bd64ab2fe07cdc2e62e0ab00
Ville <ville.v.nurmi@gmail.com>
2012-02-23 21:33:18 +0200

    REXML formats XML like Visual Studio (except some binary difference?)

--- scripts/xml_file.rb
+++ scripts/xml_file.rb
@@ -3,22 +3,47 @@ require 'pathname'
 require 'tempfile'
 require 'rexml/document'
 
+# REXML::XMLDecl doesn't allow customizing its formatting to how Visual Studio writes XML.
+class CustomXMLDecl < REXML::XMLDecl
+    def to_s; "\xEF\xBB\xBF<?xml version=\"1.0\" encoding=\"utf-8\"?>" end
+    def write(writer)
+        writer << to_s
+    end
+end
+
+# A writer that skips the first space. It just happens to be an extra space after the XML declaration.
+# Visual Studio doesn't write it, so neither will we.
+class CustomOut
+    def initialize(writer)
+        @writer = writer
+    end
+
+    def <<(value)
+        if value == " " && !@first_space
+            @first_space = true
+            return self
+        end
+        @writer << value 
+    end
+end
+
 class XMLFile
     include REXML
 
     def initialize(filepath, verbose = true)
         @verbose = verbose
         @filepath = filepath
-        @file = Document.new(IO.read(@filepath))
+        @file = Document.new(IO.read(@filepath), { :attribute_quote => :quote })
+        @file << CustomXMLDecl.new
     end
 
     def path; @filepath.to_s.gsub("/", "\\") end
 
     def save
-        formatter = Formatters::Pretty.new
+        formatter = Formatters::Pretty.new( 2, true ) # indent 2 and add a space before />
         formatter.compact = true
         new_file = Tempfile.open(["xml_file_save_temp", ".xml"])
-        formatter.write(@file, new_file)
+        formatter.write(@file, CustomOut.new(new_file))
         new_file.close
         FileUtils.mv(new_file.path, @filepath)
     end

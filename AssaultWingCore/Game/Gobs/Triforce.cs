39400a7889136dcdf2909ee0f30ad8a1226a7d17
Ville <ville.v.nurmi@gmail.com>
2012-03-17 18:43:41 +0200

    Triforce works over the network

--- AssaultWingCore/Game/Gobs/Triforce.cs
+++ AssaultWingCore/Game/Gobs/Triforce.cs
@@ -41,6 +41,7 @@ namespace AW2.Game.Gobs
         private VertexPositionTexture[] _vertexData;
         private TimeSpan _deathTime;
         private TimeSpan _nextHitTime;
+        private LazyProxy<int, Gob> _hostProxy;
 
         public override Matrix WorldMatrix
         {
@@ -50,7 +51,7 @@ namespace AW2.Game.Gobs
                 return AWMathHelper.CreateWorldMatrix(1, Host.DrawRotation + Host.DrawRotationOffset, Host.Pos + Host.DrawPosOffset);
             }
         }
-        public Gob Host { get; set; }
+        public Gob Host { get { return _hostProxy != null ? _hostProxy.GetValue() : null; } set { _hostProxy = value; } }
 
         /// <summary>
         /// Only for serialization.
@@ -116,6 +117,33 @@ namespace AW2.Game.Gobs
             {
                 pass.Apply();
                 gfx.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, _vertexData, 0, _vertexData.Length - 2);
+            }
+        }
+
+        public override void Serialize(NetworkBinaryWriter writer, SerializationModeFlags mode)
+        {
+#if NETWORK_PROFILING
+            using (new NetworkProfilingScope(this))
+#endif
+            checked
+            {
+                base.Serialize(writer, mode);
+                if (mode.HasFlag(SerializationModeFlags.VaryingDataFromServer))
+                {
+                    var hostID = Host != null ? Host.ID : Gob.INVALID_ID;
+                    writer.Write((short)hostID);
+                }
+            }
+        }
+
+        public override void Deserialize(NetworkBinaryReader reader, SerializationModeFlags mode, int framesAgo)
+        {
+            base.Deserialize(reader, mode, framesAgo);
+            if (mode.HasFlag(SerializationModeFlags.VaryingDataFromServer))
+            {
+                int hostID = reader.ReadInt16();
+                _hostProxy = new LazyProxy<int, Gob>(FindGob);
+                _hostProxy.SetData(hostID);
             }
         }
 

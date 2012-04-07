131fbc3bb94741d749c29c9c4d9e4586c526443f
Ville Nurmi <ville.v.nurmi@gmail.com>
2012-04-07 21:48:19 +0200

    Bots are excluded only between 19 and 21 Central European Time on Wednesdays.

--- scripts/aw_update_config.rb
+++ scripts/aw_update_config.rb
@@ -2,7 +2,7 @@ require 'pathname'
 def include_dir; Pathname(__FILE__).expand_path.dirname end
 require (include_dir + 'aw_config').to_s
 
-def bots_enabled; !Time.now.wednesday? end
+def bots_enabled; !Time.now.wednesday? || !(19..21).include?(Time.now.hour) end
 
 config = AWConfig.new
 config.set "//botsEnabled", bots_enabled

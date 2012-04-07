d930fc78a8445af2c04f2172fdffa0ff865ad4bd
Ville <ville.v.nurmi@gmail.com>
2012-04-07 21:57:40 +0300

    Fixed: ApplicationRevision wasn't incremented after publish

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -9,7 +9,7 @@ set FLAVOUR=%1
 call msbuild "%ROOT%\AssaultWing.sln" /target:Publish /p:Configuration=Release%FLAVOUR% /p:Platform=x86
 if ERRORLEVEL 1 goto :error
 start "" "%ROOT%\AssaultWing\bin\x86\Release%FLAVOUR%\app.publish"
-ruby "%~dp0%aw_project_file.rb" //ApplicationRevision ++
+ruby "%~dp0%aw_project_file.rb" inc //ApplicationRevision
 goto :eof
 echo Publish succeeded
 

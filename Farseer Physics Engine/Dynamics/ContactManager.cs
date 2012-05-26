d293990b1af3b0e0e2f32f3abe588125027b4294
Ville <ville.v.nurmi@gmail.com>
2012-05-26 21:00:29 +0300

    Fixed: OnSeparation event wasn't fired if the fixtures in contact were separated by their AABB's.
    http://farseerphysics.codeplex.com/workitem/31331

--- Farseer Physics Engine/Dynamics/ContactManager.cs
+++ Farseer Physics Engine/Dynamics/ContactManager.cs
@@ -287,6 +287,20 @@ namespace FarseerPhysics.Dynamics
                 if (overlap == false)
                 {
                     Contact cNuke = c;
+
+                    // http://farseerphysics.codeplex.com/workitem/31331
+                    if (cNuke.IsTouching())
+                    {
+                        // Report the separation to both participants:
+                        if (fixtureA != null && fixtureA.OnSeparation != null)
+                            fixtureA.OnSeparation(fixtureA, fixtureB);
+
+                        // Reverse the order of the reported fixtures. The first fixture is always the one that the
+                        // user subscribed to.
+                        if (fixtureB != null && fixtureB.OnSeparation != null)
+                            fixtureB.OnSeparation(fixtureB, fixtureA);
+                    }
+
                     Destroy(cNuke);
                     continue;
                 }

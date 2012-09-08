f125b93bd400a88fc3073ecf878a234703fa526b
Ville <ville.v.nurmi@gmail.com>
2012-09-08 20:21:05 +0300

    Reimplemented RunningSequenceSingle and RunningSequenceTimeSpan to create less garbage

---
+++ AWUnitTests/Helpers/Math/RunningSequenceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AW2.Helpers
+{
+    [TestFixture]
+    public class RunningSequenceTest
+    {
+        private RunningSequenceSingle _seq;
+
+        [SetUp]
+        public void Setup()
+        {
+            _seq = new RunningSequenceSingle(TimeSpan.FromSeconds(4));
+        }
+
+        [Test]
+        public void TestConstructor()
+        {
+            Assert.AreEqual(0, _seq.Count);
+            AssertCountMinMaxAverage(0, 0, 0, 0);
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            _seq.Add(42, TimeSpan.FromSeconds(1));
+            AssertCountMinMaxAverage(1, 42, 42, 42);
+            _seq.Add(22, TimeSpan.FromSeconds(2));
+            AssertCountMinMaxAverage(2, 22, 42, 32);
+            _seq.Add(92, TimeSpan.FromSeconds(3));
+            AssertCountMinMaxAverage(3, 22, 92, 52);
+        }
+
+        [Test]
+        public void TestPrune()
+        {
+            _seq.Prune(TimeSpan.FromSeconds(0));
+            AssertCountMinMaxAverage(0, 0, 0, 0);
+            _seq.Add(1, TimeSpan.FromSeconds(1));
+            _seq.Add(2, TimeSpan.FromSeconds(2));
+            _seq.Add(3, TimeSpan.FromSeconds(3));
+            _seq.Prune(TimeSpan.FromSeconds(3));
+            AssertCountMinMaxAverage(3, 1, 3, 2);
+            _seq.Add(4, TimeSpan.FromSeconds(4));
+            _seq.Add(5, TimeSpan.FromSeconds(5));
+            _seq.Prune(TimeSpan.FromSeconds(5.5));
+            AssertCountMinMaxAverage(4, 2, 5, 3.5f);
+            _seq.Prune(TimeSpan.FromSeconds(5.5));
+            AssertCountMinMaxAverage(4, 2, 5, 3.5f);
+        }
+
+        [Test]
+        public void TestClone()
+        {
+            _seq.Add(1, TimeSpan.FromSeconds(1));
+            _seq.Add(2, TimeSpan.FromSeconds(2));
+            _seq.Add(3, TimeSpan.FromSeconds(3));
+            AssertCountMinMaxAverage(3, 1, 3, 2);
+            var seq2 = _seq.Clone();
+            AssertCountMinMaxAverage(3, 1, 3, 2, seq2);
+            _seq.Add(4, TimeSpan.FromSeconds(4));
+            AssertCountMinMaxAverage(3, 1, 3, 2, seq2);
+            AssertCountMinMaxAverage(4, 1, 4, 2.5f);
+        }
+
+        private void AssertCountMinMaxAverage(int count, float min, float max, float average, RunningSequenceSingle seq = null)
+        {
+            if (seq == null) seq = _seq;
+            Assert.AreEqual(count, seq.Count);
+            Assert.AreEqual(min, seq.Min);
+            Assert.AreEqual(max, seq.Max);
+            Assert.AreEqual(average, seq.Average);
+        }
+    }
+}
--- AssaultWingCore/Helpers/Math/RunningSequence.cs
+++ AssaultWingCore/Helpers/Math/RunningSequence.cs
@@ -6,60 +6,99 @@ using System.Threading;
 
 namespace AW2.Helpers
 {
+    public class RunningSequenceTimeSpan
+    {
+        private RunningSequenceSingle _ticks;
+
+        public int Count { get { return _ticks.Count; } }
+        public TimeSpan Min { get { return TimeSpan.FromTicks((long)_ticks.Min); } }
+        public TimeSpan Max { get { return TimeSpan.FromTicks((long)_ticks.Max); } }
+        public TimeSpan Sum { get { return TimeSpan.FromTicks((long)_ticks.Sum); } }
+        public TimeSpan Average { get { return TimeSpan.FromTicks((long)_ticks.Average); } }
+
+        public RunningSequenceTimeSpan(TimeSpan valueTimeout)
+        {
+            _ticks = new RunningSequenceSingle(valueTimeout);
+        }
+
+        private RunningSequenceTimeSpan(RunningSequenceSingle ticks)
+        {
+            _ticks = ticks;
+        }
+
+        public void Add(TimeSpan value, TimeSpan entryTime)
+        {
+            _ticks.Add(value.Ticks, entryTime);
+        }
+
+        public RunningSequenceTimeSpan Prune(TimeSpan now)
+        {
+            _ticks.Prune(now);
+            return this;
+        }
+
+        public RunningSequenceTimeSpan Clone()
+        {
+            return new RunningSequenceTimeSpan(_ticks.Clone());
+        }
+    }
+
     /// <summary>
     /// Keeps track of the latest values in a running sequence and provides a few 
     /// characteristic properties of them. Is safe to add values from different threads.
     /// </summary>
-    public class RunningSequence<T>
+    public class RunningSequenceSingle
     {
         [System.Diagnostics.DebuggerDisplay("{Value} @ {EntryTime}")]
         private struct ValueData
         {
-            public T Value;
+            public float Value;
             public TimeSpan EntryTime;
         }
 
-        private ConcurrentQueue<ValueData> _values = new ConcurrentQueue<ValueData>();
+        private Queue<ValueData> _values = new Queue<ValueData>();
         private TimeSpan _valueTimeout;
-        private Func<IEnumerable<T>, T> _sum;
-        private Func<T, float, T> _divide;
         private TimeSpan _lastEntryTime;
 
         public int Count { get { return _values.Count; } }
-        public T Min { get { return RawValues.Any() ? RawValues.Min() : default(T); } }
-        public T Max { get { return RawValues.Any() ? RawValues.Max() : default(T); } }
-        public T Sum { get { return _sum(RawValues); } }
-        public T Average { get { return RawValues.Any() ? _divide(Sum, Count) : default(T); } }
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+        public float Sum { get; private set; }
+        public float Average { get { return Count == 0 ? 0 : Sum / Count; } }
 
-        private IEnumerable<T> RawValues { get { return _values.Select(x => x.Value); } }
-
-        public RunningSequence(TimeSpan valueTimeout, Func<IEnumerable<T>, T> sum, Func<T, float, T> divide)
+        public RunningSequenceSingle(TimeSpan valueTimeout)
         {
             _valueTimeout = valueTimeout;
-            _sum = sum;
-            _divide = divide;
         }
 
         /// <summary>
         /// <paramref name="entryTime"/> is assumed to be non-decreasing in subsequent calls.
         /// Failure to obey this rule leads to more or less inaccurate output values.
         /// </summary>
-        public void Add(T value, TimeSpan entryTime)
+        public void Add(float value, TimeSpan entryTime)
         {
-            _lastEntryTime = entryTime;
-            _values.Enqueue(new ValueData { Value = value, EntryTime = entryTime });
+            lock (_values)
+            {
+                _lastEntryTime = entryTime;
+                _values.Enqueue(new ValueData { Value = value, EntryTime = entryTime });
+                if (value < Min || Count == 1) Min = value;
+                if (value > Max || Count == 1) Max = value;
+                Sum += value;
+            }
         }
 
         /// <summary>
         /// Prunes out old values. Call this method every time before reading output values.
         /// Call this method only from one thread at a time. Returns self.
         /// </summary>
-        public RunningSequence<T> Prune(TimeSpan now)
+        public RunningSequenceSingle Prune(TimeSpan now)
         {
-            ValueData valueData;
-            while (_values.TryPeek(out valueData) && valueData.EntryTime + _valueTimeout <= now)
-                _values.TryDequeue(out valueData);
-            return this;
+            lock (_values)
+            {
+                while (_values.Any() && _values.Peek().EntryTime + _valueTimeout <= now) _values.Dequeue();
+                UpdateProperties();
+                return this;
+            }
         }
 
         /// <summary>
@@ -67,29 +106,36 @@ namespace AW2.Helpers
         /// added to from other threads and you want to read various output values that
         /// represent one consistent state.
         /// </summary>
-        public RunningSequence<T> Clone()
+        public RunningSequenceSingle Clone()
         {
-            var clone = new RunningSequence<T>(_valueTimeout, _sum, _divide);
-            clone._values = new ConcurrentQueue<ValueData>(_values);
-            return clone;
+            lock (_values)
+            {
+                var clone = new RunningSequenceSingle(_valueTimeout);
+                clone._values = new Queue<ValueData>(_values);
+                clone.Min = Min;
+                clone.Max = Max;
+                clone.Sum = Sum;
+                return clone;
+            }
         }
-    }
 
-    public class RunningSequenceTimeSpan : RunningSequence<TimeSpan>
-    {
-        public RunningSequenceTimeSpan(TimeSpan valueTimeout)
-            : base(valueTimeout,
-                timeSpans => new TimeSpan((long)timeSpans.Select(x => x.Ticks).Sum()),
-                (timeSpan, divisor) => new TimeSpan((long)(timeSpan.Ticks / divisor)))
+        private void UpdateProperties()
         {
-        }
-    }
-
-    public class RunningSequenceSingle : RunningSequence<float>
-    {
-        public RunningSequenceSingle(TimeSpan valueTimeout)
-            : base(valueTimeout, Enumerable.Sum, (x, divisor) => x / divisor)
-        {
+            if (!_values.Any())
+            {
+                Min = Max = Sum = 0;
+                return;
+            }
+            Min = float.MaxValue;
+            Max = float.MinValue;
+            Sum = 0;
+            foreach (var value in _values)
+            {
+                var valueValue = value.Value;
+                if (valueValue < Min) Min = valueValue;
+                if (valueValue > Max) Max = valueValue;
+                Sum += valueValue;
+            }
         }
     }
 }

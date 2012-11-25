398cd6c6456948ec48f6f5060303a24cf3971359
Ville <ville.v.nurmi@gmail.com>
2012-11-25 20:10:11 +0200

    Fixed: Triforces were killed only when their owner died. This ate a lot of extra CPU.

--- AssaultWingCore/Game/Gobs/Triforce.cs
+++ AssaultWingCore/Game/Gobs/Triforce.cs
@@ -135,6 +135,7 @@ namespace AW2.Game.Gobs
 
         public override void Update()
         {
+            base.Update();
             if (Host != null && Host.Disabled) Die();
             UpdateLocation();
             UpdateGeometry();

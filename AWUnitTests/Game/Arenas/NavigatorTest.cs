12a099257d8b93c0c0467ef0447504ec7a9edc7e
Ville <ville.v.nurmi@gmail.com>
2012-12-02 12:55:52 +0200

    Added Navigator for path finding. It uses an A* implementation by Gustavo Franco.

---
+++ AWUnitTests/Game/Arenas/NavigatorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace AW2.Game.Arenas
+{
+    [TestFixture]
+    public class NavigatorTest
+    {
+        private static readonly Vector2 DIMENSIONS = new Vector2(4, 4);
+        private Navigator _navigator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _navigator = new Navigator(DIMENSIONS, 1, x => true);
+        }
+
+        [Test]
+        public void TestEmptyArena()
+        {
+            AssertPath(Vector2.Zero, Vector2.Zero, 0,0);
+            AssertPath(Vector2.Zero, new Vector2(0.1f, 0.1f), 0,0);
+            AssertPath(Vector2.Zero, new Vector2(0.6f, 0.6f), 0,0, 1,1);
+            AssertPath(Vector2.Zero, DIMENSIONS - Vector2.One, 0,0, 1,1, 2,2, 3,3);
+        }
+
+        private void AssertPath(Vector2 from, Vector2 to, params float[] expectedXY)
+        {
+            Assert.NotNull(expectedXY);
+            Assert.That(expectedXY.Length % 2 == 0);
+            var expected = new List<Vector2>();
+            for (int i = 0; i < expectedXY.Length; i += 2) expected.Add(new Vector2(expectedXY[i], expectedXY[i + 1]));
+            var path = _navigator.GetPath(from, to).ToArray();
+            Assert.AreEqual(expected.ToArray(), path);
+        }
+    }
+}
---
+++ AssaultWingCore/Game/Arenas/Navigator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace AW2.Game.Arenas
+{
+    /// <summary>
+    /// Provides route planning for an arena.
+    /// </summary>
+    public class Navigator
+    {
+        private float _granularity;
+        private PathFinderAStar _pathFinder;
+
+        public Navigator(Vector2 dimensions, float granularity, Func<Vector2, bool> isAccessible)
+        {
+            Debug.Assert(dimensions.X > 0 && dimensions.Y > 0);
+            Debug.Assert(granularity > 0);
+            _granularity = granularity;
+            var pathFinderGrid = new byte[(int)(dimensions.X / granularity), (int)(dimensions.Y / granularity)];
+            for (var gridY = 0; gridY < pathFinderGrid.GetLength(1); gridY++)
+                for (var gridX = 0; gridX < pathFinderGrid.GetLength(0); gridX++)
+                    pathFinderGrid[gridX, gridY] = isAccessible(new Vector2(gridX, gridY) * granularity) ? (byte)1 : (byte)0;
+            _pathFinder = new PathFinderAStar(pathFinderGrid)
+            {
+                Diagonals = true,
+                HeuristicEstimate = 2,
+                HeavyDiagonals = false,
+                PunishChangeDirection = false,
+                TieBreaker = false,
+                Formula = PathFinderAStar.HeuristicFormula.Manhattan,
+                SearchLimit = 10000,
+            };
+        }
+
+        public IEnumerable<Vector2> GetPath(Vector2 from, Vector2 to)
+        {
+            Func<Vector2, System.Drawing.Point> toPoint = v => new System.Drawing.Point((int)Math.Round(v.X), (int)Math.Round(v.Y));
+            var reversePath = _pathFinder.FindPath(toPoint(from), toPoint(to));
+            if (reversePath == null) yield break;
+            for (int i = reversePath.Count - 1; i >= 0; i--) yield return new Vector2(reversePath[i].X, reversePath[i].Y);
+        }
+    }
+}

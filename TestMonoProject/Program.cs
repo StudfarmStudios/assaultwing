a3e425d9a8e4d33b88d9b0a6957f2629486f9c38
Peter Lamberg <pgithub@pelam.fi>
2022-02-26 17:59:49 +0200

    TestMonoProject builds

---
+++ TestMonoProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestMonoProject
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}

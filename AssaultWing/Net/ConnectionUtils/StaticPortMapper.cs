8f34960f24a01e8cd9bca647524a7918678bc57b
Peter Lamberg <pgithub@pelam.fi>
2022-02-27 15:10:37 +0200

    comment out StaticPortMapper.cs

--- AssaultWing/Net/ConnectionUtils/StaticPortMapper.cs
+++ AssaultWing/Net/ConnectionUtils/StaticPortMapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -71,3 +72,4 @@ namespace AW2.Net.ConnectionUtils
         }
     }
 }
+*/

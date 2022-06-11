7139718fccd922abbf31bce3af5e08f20e020d1c
Peter Lamberg <pgithub@pelam.fi>
2022-06-11 14:51:21 +0300

    Depots now upload to steam

--- scripts/steam_build.sh
+++ scripts/steam_build.sh
@@ -9,15 +9,22 @@ if [ $# -lt 1 ]; then
   echo "This is a bash script built to work on plain Linux (TBD) OR Windows drive mounted on WSL2."
   echo "Usage: $0 [Configuration]"
   echo "Configuration: 'Release' for release. If 'Debug', do developer publish."
+  echo "Configuration: 'SkipBuild', go directly steam release (USE WITH CARE! YOU MAY PUBLISH BROKEN / WRONG DEPOTS!)."
   exit 2
 elif [ $# -gt 1 ]; then
   echo 1>&2 "$0: too many arguments"
   exit 2
 fi
 
+if [[ -z "${AW_STEAM_BUILD_ACCOUNT:=}" || -z "${AW_STEAM_BUILD_PASSWORD:=}" ]]; then
+  echo "Environment variables AW_STEAM_BUILD_ACCOUNT and AW_STEAM_BUILD_PASSWORD must be defined"
+  exit 2
+fi
+
 platform_run() {
   # bash with WSL runs build commands in some weird context. OTOH building on linux is probably fine.
   # TODO: Add logic here on when to do cmd \c
+  echo cmd.exe /c "$@"
   cmd.exe /c "$@"
 }
 
@@ -27,20 +34,36 @@ CONFIGURATION=$1
 ROOT=.
 SOLUTION="AssaultWing.sln"
 
-echo Remove "*/bin/*": */bin/*
-rm -rf */bin/*
+raw_clean() {
+  echo Cleaning folders
+  echo Remove "*/bin/*": */bin/*
+  rm -rf */bin/*
+}
 
-echo dotnet restore
-platform_run dotnet restore $SOLUTION
+clean_build_setup() {
+  echo dotnet restore
+  platform_run dotnet restore $SOLUTION
 
-echo dotnet clean
-platform_run dotnet clean $SOLUTION --nologo --verbosity=quiet
+  echo dotnet clean
+  platform_run dotnet clean $SOLUTION --nologo --verbosity=quiet
+}
 
+build() {
+  # It seems we need to define build profiles to build for Linux & Mac here. https://github.com/dotnet/sdk/issues/14281#issuecomment-863499124
+  # /target:Publish
+  echo dotnet build
+  platform_run dotnet build $SOLUTION --nologo --verbosity=quiet /p:Configuration=$CONFIGURATION "/p:Platform=Any Cpu"
 
-# It seems we need to define build profiles to build for Linux & Mac here. https://github.com/dotnet/sdk/issues/14281#issuecomment-863499124
-# /target:Publish
-echo dotnet build
-platform_run dotnet build $SOLUTION --nologo --verbosity=quiet /p:Configuration=$CONFIGURATION "/p:Platform=Any Cpu"
+  echo Built: */bin/*
+}
 
-echo Built: */bin/*
+if [[ "$CONFIGURATION" != "SkipBuild" ]]; then
+  echo "Configuration=${CONFIGURATION}"
+  raw_clean
+  clean_build_setup
+  build
+else 
+  echo "Skipping cleaning and building due to CONFIGURATION=${CONFIGURATION}"
+fi
 
+platform_run 'steambuild\builder\steamcmd.exe'  +login "$AW_STEAM_BUILD_ACCOUNT" "$AW_STEAM_BUILD_PASSWORD" +run_app_build '..\..\scripts\steam_build.vdf' +quit

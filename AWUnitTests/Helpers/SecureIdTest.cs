de4520ba384c45a961bcdca817cd220e50b9c0b8
Peter Lamberg <pgithub@pelam.fi>
2022-08-04 18:50:07 +0300

    Introduce SecureId
    This will be used to prevent indentifying information of players leaking
    to other players.

---
+++ AWUnitTests/Helpers/SecureIdTest.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AW2.Helpers
+{
+    [TestFixture]
+    public class SecureIdTest
+    {
+        [Test]
+        public void TestStable()
+        {
+            var secureId = new SecureId();
+            Assert.AreEqual(secureId.MakeId("foo"), secureId.MakeId("foo"));
+            Assert.AreEqual(secureId.MakeId("bar"), secureId.MakeId("bar"));
+            Assert.AreNotEqual(secureId.MakeId("foo"), secureId.MakeId("bar"));
+        }
+
+        [Test]
+        public void TestLength()
+        {
+            var secureId = new SecureId();
+            Assert.AreEqual(24, secureId.MakeId("foo").Length);
+        }
+
+        [Test]
+        public void TestSalted()
+        {
+            var secureId1 = new SecureId();
+            var secureId2 = new SecureId();
+            Assert.AreNotEqual(secureId1.MakeId("foo"), secureId2.MakeId("foo"));
+        }
+
+        [Test]
+        public void TestSpeed()
+        {
+            var secureId = new SecureId();
+            int accumulator = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var count = 10000;
+            foreach (var i in Enumerable.Range(0, count)) {
+                accumulator += secureId.MakeId($"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa {i}").Length;
+            }
+            stopWatch.Stop();
+            // This should output something much less than 1ms. Since the server is single threaded it could stutter
+            // when a player joins if the SecureId is adjusted to be too slow.
+            Console.WriteLine($"Duration of single operation {((float)stopWatch.ElapsedMilliseconds) / count} ms");
+            Assert.AreEqual(count * 24, accumulator);
+        }
+    }
+}
---
+++ AssaultWingCore/Helpers/SecureId.cs
@@ -0,0 +1,33 @@
+using System.Security.Cryptography;
+
+namespace AW2.Helpers
+{
+    /// <summary>
+    /// Use a shared salt that is regenerated for every server (and client) instance.
+    /// This is then used to generate unique IDs from values that we don't want to
+    /// share between clients. The IDs will remain stable for as long the same server process
+    /// is running, but clients can't decode each others details.
+    /// </summary>
+    public class SecureId
+    {
+        private byte[] Salt;
+
+        private readonly int SaltSize = 128;
+
+        private readonly int IterationCount = 100; // Adjusted to be fast enough using the test SecureIdTest
+
+        private readonly int IdBytes = 16;
+
+        public SecureId() {
+            Salt = RandomNumberGenerator.GetBytes(SaltSize);
+        }
+
+        public string MakeId(string input) {
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(input, Salt);
+            pbkdf2.IterationCount = IterationCount;
+            var hashedBytes = pbkdf2.GetBytes(IdBytes);
+            var id = Convert.ToBase64String(hashedBytes);
+            return id;
+        }
+    }
+} 

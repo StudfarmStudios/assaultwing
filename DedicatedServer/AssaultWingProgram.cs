a79d2150f0a46c1e82e04abec1d807e0cb19c2ae
Peter Lamberg <pgithub@pelam.fi>
2023-07-28 18:11:22 +0300

    DedicatedServer/AssaultWingProgram: remove some cruft

--- DedicatedServer/AssaultWingProgram.cs
+++ DedicatedServer/AssaultWingProgram.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-#if WINFORMS
-using System.Windows.Forms;
-#endif
 using AW2.Core;
 using AW2.Helpers;
 using AW2.UI;
@@ -14,9 +8,6 @@ namespace AW2
 {
     public class AssaultWingProgram : IDisposable
     {
-        private const string AW_BUG_REPORT_SERVER = "assaultwing.com";
-        private const int AW_BUG_REPORT_PORT = 'A' * 256 + 'W' - 1;
-
         private static CommandLineOptions g_commandLineOptions;
         private static string[] g_errorCaptions = new[]
         {
@@ -30,39 +21,22 @@ namespace AW2
 
         public static AssaultWingProgram Instance { get; private set; }
 
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        public delegate int d_sdl_setenv(string name, string value, int overwrite);
-
-        //[DllImport("SDL2.dll")]
-        public static d_sdl_setenv SDL_setenv;
-
         [STAThread]
         public static void Main(string[] args)
         {
-            //SDL_setenv = Native.LoadFunction<d_sdl_setenv>(sdl, nameof(SDL_setenv));
-
-            // SDL_setenv("SDL_VIDEODRIVER", "dummy", overwrite: 0);
-
             Thread.CurrentThread.Name = "MainThread";
-            //if (MiscHelper.IsNetworkDeployed) Log.Write("Activation URI = '{0}'", ApplicationDeployment.CurrentDeployment.ActivationUri);
+
             g_commandLineOptions = new CommandLineOptions(Environment.GetCommandLineArgs());
-            //PostInstall.EnsureDone();
 
             using (Instance = new AssaultWingProgram())
             {
                 Instance.Run();
             }
-
         }
 
         public AssaultWingProgram()
         {
             Log.Write("Assault Wing started");
-
-            // TODO: Peter: unhandled exception handling and reporting:
-            // something like:
-            // System.AppDomain.CurrentDomain.UnhandledException += ThreadExceptionHandler;
-
             _form = new GameForm(g_commandLineOptions);
         }
 
@@ -81,15 +55,11 @@ namespace AW2
         {
             if (_form != null) _form.Dispose();
             _form = null;
-            // PETER MOD: Remove exception handler
-            //Application.ThreadException -= ThreadExceptionHandler;
         }
 
         private void ThreadExceptionHandler(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
-            _form.ForceCursorShown();
             _form.FinishGame();
-            _form.SetWindowed();
             ReportException(e.Exception);
             Exit();
         }
@@ -105,30 +75,7 @@ namespace AW2
             var report = string.Format("Assault Wing {0}\nCrashed on {1:u}\nHost {2}\n\n{3}",
                 MiscHelper.Version, DateTime.Now.ToUniversalTime(), Environment.MachineName, e.ToString());
 
-            // TODO: Peter: Report exception to the user
-#if WINFORMS
-            var result = g_commandLineOptions.DedicatedServer
-                ? DialogResult.Yes
-                : MessageBox.Show(intro + "\n\n" + report, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Error);
-            var logHeader = "\n\n*** Assault Wing run log ***\n\n";
-            if (result == DialogResult.Yes) SendMail(report + logHeader + Log.CloseAndGetContents());
-
-            // Raise a Windows event to notify any dedicated server keepalive task to relaunch the server.
-            System.Diagnostics.EventLog.WriteEvent("Application Error", new System.Diagnostics.EventInstance(1000, 2), "AssaultWing.exe");
-#endif
-
             g_reportingException = false;
-        }
-
-        private static void SendMail(string text)
-        {
-            var tcpClient = new TcpClient();
-            tcpClient.Connect(AW_BUG_REPORT_SERVER, AW_BUG_REPORT_PORT);
-            var data = Encoding.UTF8.GetBytes(text);
-            var tcpStream = tcpClient.GetStream();
-            tcpStream.Write(BitConverter.GetBytes(System.Net.IPAddress.HostToNetworkOrder(data.Length)), 0, sizeof(int));
-            tcpStream.Write(data, 0, data.Length);
-            tcpClient.Close();
         }
     }
 }
